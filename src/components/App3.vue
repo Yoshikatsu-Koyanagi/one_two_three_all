7c29a404507d38cd71cbb9feb6185cb823effbf2
Yoshikatsu-Koyanagi <soccer211554@i.softbank.jp>
2021-01-04 16:46:56 +0900

    commit

---
+++ src/components/App3.vue
@@ -0,0 +1,110 @@
+<template>
+   <canvas ref="canv" :width="width" :height="height"></canvas>
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+export default { 
+  props: ["width","height","bg_c_2","bg_c_3"],
+  data() {
+    return {
+      num_width_ratio: 0.8,
+      num_height_ratio: 0.5,
+      flame_weight_ratio: 0.01, //キャンバスの横幅に対するボタンの枠の太さの割合
+      font_ratio: 0.8, //数字ボタンの高さに対するフォントサイズの割合
+      font_name: "meirio", //フォントの書体
+      r: 10, //数字ボタンの枠の角の半径
+    }
+  },
+  methods: {
+    draw_3_btn(num_color){
+      //数字ボタンの枠
+      this.context.globalCompositeOperation = "destination-out";
+      this.context.beginPath();
+      this.context.moveTo(this.num_side_margin+this.r,this.num_top_margin);
+      this.context.arc(this.num_side_margin+this.r, this.num_top_margin+this.r, this.r, Math.PI*1.5, Math.PI, true);  
+      this.context.lineTo(this.num_side_margin, this.num_top_margin+this.num_height-this.r);
+      this.context.arc(this.num_side_margin+this.r, this.num_top_margin+this.num_height-this.r, this.r, Math.PI, Math.PI*0.5, true); 
+      this.context.lineTo(this.num_side_margin+this.num_width-this.r,this.num_top_margin+this.num_height);
+      this.context.arc(this.num_side_margin+this.num_width-this.r, this.num_top_margin+this.num_height-this.r, this.r, Math.PI*0.5, Math.PI*0, true); 
+      this.context.lineTo(this.num_side_margin+this.num_width,this.num_top_margin+this.r);
+      this.context.arc(this.num_side_margin+this.num_width-this.r, this.num_top_margin+this.r, this.r, Math.PI*0, Math.PI*1.5, true); 
+      this.context.closePath();
+      this.context.fillStyle = "rgba(0,0,0,1)";
+      this.context.fill();
+
+      this.context.globalCompositeOperation = "source-over";
+      this.context.fillStyle = num_color;
+      this.context.fill();
+
+      //数字ボタンの"数字"
+      this.context.font = this.font;
+      this.context.fillStyle = "rgba(0,0,0,1)";
+      this.context.textAlign = "center";
+      this.context.fillText(3, this.nbw*0.5, this.num_top_margin+this.num_height*0.8,this.num_width);
+    
+      this.context.lineWidth = this.flame_weight;	
+      this.context.strokeStyle = "rgb(0,0,0)"; 
+      this.context.stroke();
+    },
+  },
+  mounted() {
+    this.flame_weight = this.width*this.flame_weight_ratio //ボタンの枠の太さ
+
+    this.nbw = this.width;
+    this.nbh = this.height;
+    
+    this.num_width = this.nbw*this.num_width_ratio;
+    this.num_height = this.nbh*this.num_height_ratio;
+    this.num_side_margin = this.nbw*(1-this.num_width_ratio)*0.5;
+    this.num_top_margin = this.nbh*(1-this.num_height_ratio)*0.5;
+   
+    let c = this.$refs.canv;
+    this.context = c.getContext('2d');
+
+    this.gradient_b = this.context.createLinearGradient(0, this.nbh*0.5, this.nbw, this.nbh*0.5);
+    this.gradient_b.addColorStop(0.0 , this.bg_c_3);
+    this.gradient_b.addColorStop(1.0 , this.bg_c_2);
+    this.context.fillStyle = this.gradient_b;
+    this.context.fillRect(0,0,this.nbw,this.nbh);  
+    
+    //数字ボタンを作成
+    this.font_size = this.num_height*this.font_ratio; //フォントサイズ
+    this.font = `${this.font_size}px ${this.font_name}`; //フォントサイズと書体    
+    let num_color; //数字ボタンの色
+    this.gradient = this.context.createLinearGradient(this.nbw*0.5,this.num_top_margin,this.nbw*0.5,this.num_top_margin+this.num_height);
+    this.gradient.addColorStop(0.0 , 'rgba(0,0,0,0.35)');
+    this.gradient.addColorStop(0.8 , 'rgba(0,0,0,0.05)');
+    num_color = this.gradient;
+    this.draw_3_btn(num_color);
+    let num_down = false;
+
+    c.onmousedown = (e) => {
+          let gradient2 = this.context.createLinearGradient(this.nbw*0.5,this.num_top_margin,this.nbw*0.5,this.num_top_margin+this.num_height);
+          gradient2.addColorStop(0.0 , 'rgba(0,0,0,0.5)');
+          gradient2.addColorStop(0.8 , 'rgba(0,0,0,0.3)');
+          num_color = gradient2;
+          this.draw_3_btn(num_color);
+          this.$emit("click3");
+          return num_down = true;
+    }
+  
+    c.onmouseup = (e) => {
+        num_color = this.gradient;
+        this.draw_3_btn(num_color);  
+    } 
+        //数字ボタンが押されて且つ数字ボタン上で離されたとき        
+    if (num_down == true) {
+    }
+  
+    c.onmouseout = (e) => {
+        num_color = this.gradient;
+        this.draw_3_btn(num_color);
+    }       
+  },
+}
+</script>
---
+++ src/library-main.js
@@ -0,0 +1,32 @@
+// コンポーネントのインポート
+import component1 from './components/App1.vue'; //(1)Vueのファイル名に変更
+import component2 from './components/App2.vue';
+import component3 from './components/App3.vue';
+import componentAll from './components/AppAll.vue';
+
+
+// Vue.use() によって実行される install 関数を定義
+export function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+}
+
+// Vue.use() のためのモジュール定義を作成
+// Create module definition for Vue.use()
+const plugin = {
+    install,
+};
+
+// vue が見つかった場合に自動インストールする (ブラウザで <script> タグを用いた場合等)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+// (npm/webpack 等で) モジュールとして利用させるためコンポーネントを export する
+export {component1, component2, component3, componentAll};
